ac24855c20c3d662866a138054165f289cd25917
shawn <shawnantonucci2018@gmail.com>
2019-04-02 23:25:04 -0700

    signup and login

--- reactNative-firebase/App.js
+++ reactNative-firebase/App.js
@@ -26,13 +26,54 @@ import {
 } from "native-base";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  signUpUser = (email, password) => {
+
+    try{
+
+      if(this.state.password.length < 6) {
+        alert("Please enter at least 6 characters")
+        return;
+      }
+
+      firebase.auth().createUserWithEmailAndPassword(email, password);
+    }
+    catch(error) {
+      console.log(error.toString())
+    }
+  };
+
+  logInUser = (email, password) => {
+
+    try {
+      firebase.auth().signInWithEmailAndPassword(email, password).then(user => {
+        console.log(user);
+      })
+    }
+    catch(error) {
+      console.log(error.toString())
+    }
+  };
+
   render() {
     return (
       <Container style={styles.container}>
         <Form>
           <Item floatingLabel>
             <Label>Email</Label>
-            <Input autoCorrect={false} autoCapitalize="none" />
+            <Input
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={email => this.setState({ email })}
+            />
           </Item>
 
           <Item floatingLabel>
@@ -41,10 +82,30 @@ export default class App extends React.Component {
               secureTextEntry={true}
               autoCorrect={false}
               autoCapitalize="none"
+              onChangeText={password => this.setState({ password })}
             />
           </Item>
-          <Button style={{ marginTop: 10 }} full rounded success>
-            <Text>Login</Text>
+          <Button
+            style={{ marginTop: 10 }}
+            full
+            rounded
+            success
+            onPress={() =>
+              this.logInUser(this.state.email, this.state.password)
+            }
+          >
+            <Text style={{ color: "white" }}>Login</Text>
+          </Button>
+          <Button
+            style={{ marginTop: 10 }}
+            full
+            rounded
+            primary
+            onPress={() =>
+              this.signUpUser(this.state.email, this.state.password)
+            }
+          >
+            <Text style={{ color: "white" }}>Sign Up</Text>
           </Button>
         </Form>
       </Container>
